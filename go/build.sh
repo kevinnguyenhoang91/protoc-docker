8669fea9492f607b96481d04214b28ff12eeb6ff
Roger Chapman <roger.chapman@safetyculture.io>
2018-08-07 13:42:43 +1000

    Add Gogo tools

--- go/build.sh
+++ go/build.sh
@@ -27,6 +27,8 @@ done
 
 mkdir -p "$GOPATH/src" "$GOPATH/bin" && chmod -R 777 "$GOPATH"
 go get -u github.com/golang/protobuf/protoc-gen-go
+go get -u github.com/gogo/protobuf/protoc-gen-gogo
+go get -u github.com/mwitkow/go-proto-validators/protoc-gen-govalidators
 go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
 go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
 
