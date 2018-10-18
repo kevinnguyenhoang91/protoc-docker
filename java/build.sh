fd7f2278ad8bfd9b15bc4d2b28fc8deb49db6dec
rogchap <roger.chapman@safetyculture.io>
2018-10-18 13:55:34 +1100

    Add support for java and javalite

---
+++ java/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -e
+
+apk add --no-cache --virtual .build g++
+
+# Install the protoc-gen-grpc-java plugin
+mkdir -p /usr/local/grpc
+curl -L https://github.com/grpc/grpc-java/archive/v${GRPC_JAVA_VERSION}.tar.gz | tar xvz --strip-components=1 -C /usr/local/grpc
+cd /usr/local/grpc/compiler/src/java_plugin/cpp
+g++ -I. -I/protobuf/src *.cpp -L/protobuf/src/.libs -lprotoc -lprotobuf -lpthread --std=c++0x -s -o protoc-gen-grpc-java
+install -c protoc-gen-grpc-java /usr/local/bin/
+
+apk del .build
