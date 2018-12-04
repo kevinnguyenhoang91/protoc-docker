f03e07e94773800e06c811f6f559b475b5c1f5d6
Roger Chapman <rogchap@gmail.com>
2018-12-04 15:00:57 +1100

    Temp fix for typescript enums (#3)

--- web/build.sh
+++ web/build.sh
@@ -5,11 +5,15 @@ set -e
 apk add --no-cache libstdc++
 apk add --no-cache --virtual .build git openssl binutils-gold g++ gcc gnupg libgcc linux-headers make cmake python autoconf automake libtool
 
+# //TODO: [RC] Waiting for https://github.com/grpc/grpc-web/pull/404 to land before going back to official release
+
 # Install the protoc-gen-grpc-web
 mkdir -p /usr/local/grpc-web
-git clone https://github.com/grpc/grpc-web.git /usr/local/grpc-web
+# git clone https://github.com/grpc/grpc-web.git /usr/local/grpc-web
+git clone https://github.com/SafetyCulture/grpc-web.git /usr/local/grpc-web
 cd /usr/local/grpc-web
-git checkout $GRPC_WEB_RELEASE
+# git checkout $GRPC_WEB_RELEASE
+git checkout typescript-enums
 make install-plugin
 
 apk del .build
