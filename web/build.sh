341c7aef643f3ed74cb5caf669099e46aefb2dac
Roger Chapman <roger.chapman@safetyculture.io>
2019-01-29 10:51:49 +1100

    Update to latest merge in forked master

--- web/build.sh
+++ web/build.sh
@@ -5,7 +5,7 @@ set -e
 apk add --no-cache libstdc++
 apk add --no-cache --virtual .build git openssl binutils-gold g++ gcc gnupg libgcc linux-headers make cmake python autoconf automake libtool
 
-# //TODO: [RC] Waiting for https://github.com/grpc/grpc-web/pull/404 to land before going back to official release
+# //TODO: [RC] Waiting for an offical release to land before going back to official repo.
 
 # Install the protoc-gen-grpc-web
 mkdir -p /usr/local/grpc-web
@@ -13,7 +13,7 @@ mkdir -p /usr/local/grpc-web
 git clone https://github.com/SafetyCulture/grpc-web.git /usr/local/grpc-web
 cd /usr/local/grpc-web
 # git checkout $GRPC_WEB_RELEASE
-git checkout es5-functions
+git checkout master
 make install-plugin
 
 apk del .build
