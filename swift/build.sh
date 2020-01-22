eda69a3f82550070bcd90846af82711e22d05f3a
Roger Chapman <roger.chapman@safetyculture.io>
2020-01-22 13:31:23 +1100

    update to swift 5 and nio grpc (#29)

--- swift/build.sh
+++ swift/build.sh
@@ -9,16 +9,16 @@ apt-get -q install -y libz-dev unzip patch patchelf
 mkdir -p /usr/local/grpc
 git clone https://github.com/grpc/grpc-swift /usr/local/grpc
 cd /usr/local/grpc
-make
-cd /
+git checkout $GRPC_RELEASE
+make plugin
 
 mkdir -p /plugins
-cp /usr/local/grpc/protoc-gen-swift /usr/local/grpc/protoc-gen-swiftgrpc /plugins
+cp /usr/local/grpc/protoc-gen-swift /usr/local/grpc/protoc-gen-grpc-swift /plugins
 
 cp /lib64/ld-linux-x86-64.so.2 $(ldd /plugins/protoc-gen-swift | awk '{print $3}' | grep /lib | sort | uniq) /plugins/
 
 find /plugins/ -name 'lib*.so*' -exec patchelf --set-rpath /plugins {} \;
 
-for p in protoc-gen-swift protoc-gen-swiftgrpc; do
+for p in protoc-gen-swift protoc-gen-grpc-swift; do
   patchelf --set-interpreter /plugins/ld-linux-x86-64.so.2 --set-rpath /plugins /plugins/${p}; \
 done
