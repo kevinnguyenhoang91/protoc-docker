46889108e063bc84659129f028dc5c8c90f6c37f
Brandon Cook <BCook98@users.noreply.github.com>
2022-03-17 12:33:03 +1100

    [INTG-2119] Update protoc-gen-openapiv2 back to original instead of fork (#89)
    * [INTG-2119] Update protoc-gen-openapiv2 back to original instead of fork
    
    Also changes the build process to copy prebuilt go binaries from the alpine image instead of building them from source every time.
    
    * Update go to 1.18

--- go/build.sh
+++ go/build.sh
@@ -2,24 +2,14 @@
 
 set -e
 
-apk add --no-cache --virtual .go_build bash gcc musl-dev openssl go git
-
-curl -o go.tgz -L https://dl.google.com/go/go$GOLANG_VERSION.src.tar.gz
-
-echo "$GOLANG_CHECKSUM *go.tgz" | sha256sum -c -
-tar -C /usr/local -xzf go.tgz
-rm go.tgz
-
-cd /usr/local/go/src
-./make.bash
-
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 go install github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto@latest
 
 # Includes HTTP Pattern fix that is in main branch but yet to be included in a release
 # Once included we can revert back to using @latest or better still @2.x
-go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@c0ecb42
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@7076625
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@7076625
 
 go install github.com/SafetyCulture/s12-proto/protobuf/protoc-gen-govalidator@v$S12_PROTO_VERSION
 go install github.com/SafetyCulture/s12-proto/protobuf/protoc-gen-s12perm@v$S12_PROTO_VERSION
@@ -28,13 +18,6 @@ go install github.com/SafetyCulture/protoc-gen-workato/cmd/protoc-gen-workato@la
 
 go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@latest
 
-# This is a fork, and go install hates forks...
-curl -o grpc-gateway.tar.gz -L https://github.com/SafetyCulture/grpc-gateway/archive/refs/heads/sc-main.tar.gz
-tar -xzf grpc-gateway.tar.gz
-cd grpc-gateway-sc-main/
-go install ./protoc-gen-openapiv2
-
 rm -rf $GOPATH/pkg/mod
-apk del .go_build
 
 go version
