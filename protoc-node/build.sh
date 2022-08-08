c521dc3d6815505043168971a8066816ecfc9ec0
Scott Gangemi <scottgangemi@gmail.com>
2022-08-08 12:50:20 +1000

    [IS-2233] Add in `buf` tools to Node image, bump version (#109)

--- protoc-node/build.sh
+++ protoc-node/build.sh
@@ -15,6 +15,7 @@ make grpc_node_plugin
 mv /usr/local/grpc/bins/opt/grpc_node_plugin  /usr/local/bin/grpc_node_plugin
 
 npm install -g ts-protoc-gen
+npm install @bufbuild/protoc-gen-es @bufbuild/protoc-gen-connect-web
 
 apk del .build
 rm -rf /usr/local/grpc
